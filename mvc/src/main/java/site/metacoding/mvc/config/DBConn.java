bd549f5f38f1623f626f87f0611acb8b9f581bf8
milo <mybule8@naver.com>
2022-02-24 12:50:56 +0900

    mvc패턴구조설정완료

---
+++ mvc/src/main/java/site/metacoding/mvc/config/DBConn.java
@@ -0,0 +1,26 @@
+package site.metacoding.mvc.config;
+
+import java.sql.Connection;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class DBConn {
+	
+	public static Connection getConnection() {
+		Connection conn = null;
+		try {
+			Context initContext = new InitialContext();
+			Context envContext  = (Context)initContext.lookup("java:/comp/env");
+			DataSource ds = (DataSource)envContext.lookup("jdbc/myoracle");
+			conn = ds.getConnection();
+			System.out.println("DB연결성공");
+			//etc.	
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("DB연결실패");
+		}
+		return conn;
+	}
+}
